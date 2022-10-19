d0f0982d7c1258761fe5ef050eee4c7b5c44cd25
KlayVonDowe <70662432+KlayVonDowe@users.noreply.github.com>
2022-10-18 20:55:06 -0500

    push

--- index.js
+++ index.js
@@ -27,28 +27,36 @@ fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/maste
     .attr('width',(d) => xScale.bandwidth(d.year))
     .attr('height', (d) => yScale.bandwidth(d.month))
     .attr('data-year',(d) => d.year)
-    .attr('data-month',(d) => d.month)
+    .attr('data-month',(d) => d.month - 1)
     .attr('data-temp',(d) => (temp - (d.variance)))
     .attr('class','cell')
-    .attr('fill', (item) => {
-        let variance = item['variance']
-        if(variance <= -1){
-            return 'SteelBlue'
-        }else if(variance <= 0){
-            return 'LightSteelBlue'
-        }else if(variance <= 1){
-            return 'Orange'
-        }else{
-            return 'Crimson'
-        }
+    //.attr('fill', (item) => {
+    //    let variance = item['variance']
+     //   if(variance <= -1){
+     //       return 'SteelBlue'
+     //   }else if(variance <= 0){
+      //      return 'LightSteelBlue'
+     //   }else if(variance <= 1){
+      //      return 'Orange'
+      //  }else{
+       //     return 'Crimson'
+       // }
+   // })
+    .attr('fill', (d) => {
+      return  d3.scaleThreshold().domain((d) => temp-d.variance).range('SteelBlue', 'LightSteelBlue','Orange','Crimson')
     })
 
-    const xAxis = d3.scaleTime().domain(d3.extent(Data, (d) => d.year)).range([60, w-p])
-    const bottomAxis = d3.axisBottom(xAxis)
+    const xAxis = d3.scaleLinear().domain([d3.min(Data.map((d) => d.year)), d3.max(Data.map((d) => d.year))]).range([60, w-p])
+    const bottomAxis = d3.axisBottom(xAxis).tickFormat(d3.format('d'))
     svg.append('g').attr('id','x-axis').attr('transform' , 'translate(0,' + (h- 60) + ')').call(bottomAxis)
 
-    const yAxis = d3.scaleTime().domain(d3.extent(Data, (d) => d.month)).range([60, h-p])
-    const leftAxis = d3.axisLeft(yAxis)
+    const yAxis =d3.scaleBand().domain(Data.map((d) => d.month)).range([60, h-p])
+    const leftAxis = d3.axisLeft(yAxis).tickFormat(function (month) {
+        var date = new Date(0);
+        date.setUTCMonth(month);
+        var format = d3.timeFormat('%B');
+        return format(date);
+      })
     svg.append('g').attr('id','y-axis').attr('transform','translate(60,0)').call(leftAxis)
 
 })
